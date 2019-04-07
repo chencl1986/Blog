45f8a7d4ba4a81c4602bd152a8bc9f66f0c75825
chencl1986 <chencl1986@qq.com>
2019-04-07 23:59:32 +0800

    Express教程05：Cookie

--- Demos/Express入门系列/lesson05/server.js
+++ Demos/Express入门系列/lesson05/server.js
@@ -14,16 +14,37 @@ server.use(cookieParser(
 server.get('/cookie', (req, res, next) => {
   // express自带的设置Cookie方法
   res.cookie('userName', 'lee', {
-    // 设置Cookie有效期
-    expires: new Date(new Date().getTime() + 14 * 86400000)
+    // 设置该Cookie只可以由服务端访问，即前端JavaScript无法访问document.cookie获取该值，但控制台还是可以查看和修改
+    httpOnly: true,
+    // 只有通过HTTPS请求的Cookie才被使用，否则都认为是错误的Cookie
+    // secure: true,
+    // 设置保存Cookie的域名，浏览器查找Cookie时，子域名（如translate.google.com）可以访问主域名（google.com）下的Cookie，而主域名（google.com）不可以访问子域名（如translate.google.com）下的Cookie
+    // 本地测试可直接设置为localhost
+    domain: 'localhost',
+    // 设置保存Cookie的路径，浏览器查找Cookie时，子路径（如/map）可以访问根路径（'/'）下设置的Cookie，而根路径（'/'）无法访问子路径（如/map）下设置的Cookie
+    path: '/',
+    // 通过expires设置Cookie过期时间为14天后
+    // expires: new Date(new Date().getTime() + 14 * 86400000),
+    // 通过maxAge设置Cookie过期时间为14天后
+    maxAge: 14 * 86400000,
+  })
+
+  res.cookie('password', 'test123', {
+    httpOnly: true,
+    domain: 'localhost',
+    path: '/',
+    maxAge: 14 * 86400000,
+    // 开启该Cookie的签名模式
+    signed: true
   })
 
   // 读取cookieParser解析的Cookie
-  console.log(req.cookies)
+  console.log(`cookies: ${JSON.stringify(req.cookies)}`)
 
-  res.send('Cookie: ' + JSON.stringify(req.cookies))
+  // 经过签名的Cookie
+  console.log(`signedCookies: ${JSON.stringify(req.signedCookies)}`)
 
-  
+  res.send(`cookies: ${JSON.stringify(req.cookies)}. signedCookies: ${JSON.stringify(req.signedCookies)}`)
 })
 
 server.use(express.static('./static/'))
